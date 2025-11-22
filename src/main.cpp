4f5c7c5b0af0f4dbfd24ac72dbcf22f412dbd166
Elisa <ciardulli.elisa@gmail.com>
2025-11-22 14:31:02 +0100

    improved platforms

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_RESET -1
-//#define SCREEN_ADDRESS 0x3D //Physical
+// #define SCREEN_ADDRESS 0x3D //Physical
 #define SCREEN_ADDRESS 0x3C // Emulator
 
 Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
@@ -15,14 +15,15 @@ Adafruit_SSD1306 oled(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 #define SCL_PIN 22
 
 // Basic variables. Necessary for the rest of the variables to work.
-const int PADDING = 1; // space between character and platform               
-const int PLATFORM_HEIGHT = 4;      
-const int PLATFORM_INTERVAL = 16;   
+const int PADDING = 1; // space between character and platform
+const int PLATFORM_HEIGHT = 4;
+const int PLATFORM_INTERVAL = 16;
 const int JUMP = PLATFORM_INTERVAL + PLATFORM_HEIGHT;
 const int UNIT = 8;
-const int MAX_PLATFORMS = 16;        
+const int MAX_PLATFORMS = 16;
 
-struct Platform {
+struct Platform
+{
   int x;
   int y;
   int length;
@@ -30,8 +31,8 @@ struct Platform {
 };
 
 // Platform variables
-int speed = 1; // speed at which the platforms move            
-int lastX;     // the end of the furthest left platform           
+int speed = 1; // speed at which the platforms move
+int lastX;     // the end of the furthest left platform
 
 // Array containing platforms
 Platform platforms[MAX_PLATFORMS];
@@ -39,9 +40,8 @@ Platform platforms[MAX_PLATFORMS];
 // Array containing all the possible heights at which a platform could be
 const int MAX_LEVELS = 2;
 int levels[MAX_LEVELS] = {
-  SCREEN_HEIGHT - PLATFORM_HEIGHT,
-  SCREEN_HEIGHT - PLATFORM_HEIGHT - JUMP
-};
+    SCREEN_HEIGHT - PLATFORM_HEIGHT,
+    SCREEN_HEIGHT - PLATFORM_HEIGHT - JUMP};
 
 // Hardware logic variables
 int pinButtonUp = 13;
@@ -54,14 +54,13 @@ const int heartHeight = 7;
 int lives = 3;
 
 const unsigned char heartBitmap[] PROGMEM = {
-  0b01100110,
-  0b11111111,
-  0b11111111,
-  0b11111111,
-  0b01111110,
-  0b00111100,
-  0b00011000
-};
+    0b01100110,
+    0b11111111,
+    0b11111111,
+    0b11111111,
+    0b01111110,
+    0b00111100,
+    0b00011000};
 
 // Player variables
 const int CHARACTER_WIDTH = 8;
@@ -70,43 +69,42 @@ int xCharacter;
 int yCharacter;
 
 const unsigned char ghostBitmap[] PROGMEM = {
-  0b00111100, 
-  0b01111110, 
-  0b11111111, 
-  0b11011011, 
-  0b11011011, 
-  0b11011011,
-  0b11111111, 
-  0b11110111, 
-  0b11100111, 
-  0b11000011  
-};
+    0b00111100,
+    0b01111110,
+    0b11111111,
+    0b11011011,
+    0b11011011,
+    0b11011011,
+    0b11111111,
+    0b11110111,
+    0b11100111,
+    0b11000011};
 
 // Physics variables to handle jumps
-float ySpeed = 0;     // vertical speed
-float gravity = 0.8;  // gravity applied when falling down
-float impulse = -7;   // upward impulse
+float ySpeed = 0;   // vertical speed
+float gravity = 1;  // gravity applied when falling down
+float impulse = -8; // upward impulse
 
 bool isJumping = false;
 bool isFalling = false;
 
 // enemy variables
 const unsigned char enemyBitmap[] PROGMEM = {
-    0b00011000, // ...##...
-    0b00111100, // ..####..
-    0b01111110, // .######.
-    0b11111100, // ######.. <-- Mouth here
-    0b11111000, // #####... <-- Mouth here
-    0b11111100, // ######.. <-- Mouth here
-    0b01111110, // .######.
-    0b00111100, // ..####..
-    0b00011000, // ...##...
-    0b00000000  // ........
-};
+    0b00011000, 0b00,
+    0b00111100, 0b00,
+    0b01111110, 0b00,
+    0b11011011, 0b00,
+    0b11111111, 0b00,
+    0b10111101, 0b00,
+    0b10100101, 0b00,
+    0b11100111, 0b00,
+    0b01000010, 0b00,
+    0b10100101, 0b00};
+
 int xEnemy;
 int yEnemy;
 int ENEMY_HEIGHT = 10;
-int ENEMY_WIDTH = 8;
+int ENEMY_WIDTH = 10;
 
 // function to draw the hearts
 void drawHearts(int lives)
@@ -127,47 +125,55 @@ void drawEnemy()
 }
 
 // function to draw the player
-void drawGhost() {
+void drawGhost()
+{
   oled.drawBitmap(xCharacter, yCharacter, ghostBitmap, CHARACTER_WIDTH, CHARACTER_HEIGHT, WHITE);
 }
 
-// When the UP button is clicked, the player jumps up 
-void jump() {
-  if (digitalRead(pinButtonUp) == LOW && prevPinButtonUp == HIGH) {
-    if (!isFalling) {     // player can jump only if on a platform
-      ySpeed = impulse;    
-      isFalling = true;           
+// When the UP button is clicked, the player jumps up
+void jump()
+{
+  if (digitalRead(pinButtonUp) == LOW && prevPinButtonUp == HIGH)
+  {
+    if (!isFalling)
+    { // player can jump only if on a platform
+      ySpeed = impulse;
+      isFalling = true;
       isJumping = true;
     }
   }
 }
 
 // applying physics logic to the jump
-void jumpPhysics() {
-  
+void jumpPhysics()
+{
+
   // apply gravity to the player
   ySpeed += gravity;
   yCharacter += ySpeed;
 
   bool landed = false;
   // checking for collisions on platforms
-  for (int i = 0; i < MAX_PLATFORMS; i++) {
-    if (!platforms[i].visible) {
+  for (int i = 0; i < MAX_PLATFORMS; i++)
+  {
+    if (!platforms[i].visible)
+    {
       continue;
-    } 
+    }
 
     // Check horizontal overlap between character feet and platform
     bool horizontalOverlap =
-      (xCharacter + CHARACTER_WIDTH > platforms[i].x) &&
-      (xCharacter < platforms[i].x + platforms[i].length);
+        (xCharacter + CHARACTER_WIDTH > platforms[i].x) &&
+        (xCharacter < platforms[i].x + platforms[i].length);
 
     // Check if character feet intersect platform top
     bool feetTouchPlatform =
-      (yCharacter + CHARACTER_HEIGHT >= platforms[i].y - PADDING) &&
-      (yCharacter + CHARACTER_HEIGHT <= platforms[i].y + PLATFORM_HEIGHT);
+        (yCharacter + CHARACTER_HEIGHT >= platforms[i].y - PADDING) &&
+        (yCharacter + CHARACTER_HEIGHT <= platforms[i].y + PLATFORM_HEIGHT);
 
     // condition for player to land on platform
-    if (horizontalOverlap && feetTouchPlatform && ySpeed >= 0) {
+    if (horizontalOverlap && feetTouchPlatform && ySpeed >= 0)
+    {
       yCharacter = platforms[i].y - CHARACTER_HEIGHT - PADDING;
       ySpeed = 0;
       isFalling = false;
@@ -178,17 +184,21 @@ void jumpPhysics() {
   }
 
   // no collision so player keeps falling down
-  if (!landed) {
+  if (!landed)
+  {
     isFalling = true;
   }
 }
 
 // respawn the player on the first ground platform available
-void repositionPlayer() {
-  for (int i = 0; i < MAX_PLATFORMS; i++) {
-    if (platforms[i].visible && platforms[i].y == levels[0]) { // ground platform identified
-      xCharacter = platforms[i].x + 4; 
-      yCharacter = platforms[i].y - CHARACTER_HEIGHT - PADDING; 
+void repositionPlayer()
+{
+  for (int i = 0; i < MAX_PLATFORMS; i++)
+  {
+    if (platforms[i].visible && platforms[i].x == 0)
+    { // ground platform identified
+      xCharacter = platforms[i].x + 4;
+      yCharacter = platforms[i].y - CHARACTER_HEIGHT - PADDING;
       ySpeed = 0;
       isFalling = false;
       isJumping = false;
@@ -198,17 +208,21 @@ void repositionPlayer() {
 }
 
 // handle player falling and losing lives
-void handleFallAndLives() {
-  if (yCharacter > SCREEN_HEIGHT) { 
-    if (lives > 0) {
-      lives--;          
-      repositionPlayer(); 
+void handleFallAndLives()
+{
+  if (yCharacter > SCREEN_HEIGHT)
+  {
+    if (lives > 0)
+    {
+      lives--;
+      repositionPlayer();
     }
   }
 }
 
 // game ends
-void gameOver() {
+void gameOver()
+{
   oled.clearDisplay();
   oled.setTextSize(2);
   oled.setTextColor(WHITE);
@@ -219,7 +233,8 @@ void gameOver() {
   oled.display();
   delay(30);
 
-  if (digitalRead(pinButtonUp) == LOW) {
+  if (digitalRead(pinButtonUp) == LOW)
+  {
     // Reset lives
     lives = 3;
 
@@ -229,8 +244,9 @@ void gameOver() {
     yCharacter = levels[0] - CHARACTER_HEIGHT;
 
     // Reset platform array
-    for (int i = 0; i < MAX_PLATFORMS; i++) {
-        platforms[i].visible = false;
+    for (int i = 0; i < MAX_PLATFORMS; i++)
+    {
+      platforms[i].visible = false;
     }
 
     // regenerate initial ground platform
@@ -243,24 +259,29 @@ void gameOver() {
     lastX = start.x + start.length;
     xCharacter = start.x + 4;
   }
-} 
+}
 
 // Create a platform when possible
-void createPlatform() {
+void createPlatform()
+{
 
   // Find first available slot in the platforms array
-  for (int i = 0; i < MAX_PLATFORMS; i++) {
+  for (int i = 0; i < MAX_PLATFORMS; i++)
+  {
     // stop if the generated platforms fill the screen width
-    if (lastX < SCREEN_WIDTH) {
-      if (!platforms[i].visible) {
+    if (lastX < SCREEN_WIDTH)
+    {
+      if (!platforms[i].visible)
+      {
         // There is a  lesser chance of there being a gap
-        int randomX = random(6) == 0 ? 1 : 0;
+        int randomX = random(10) == 0 ? 1 : 0;
         platforms[i].x = lastX + UNIT * randomX;
-        platforms[i].length = UNIT;
+        platforms[i].length = UNIT * random(1, 5);
         platforms[i].y = levels[random(MAX_LEVELS)];
         platforms[i].visible = true;
         // Update last x, if the new platform is further right than any other platform
-        if (lastX <= platforms[i].x + platforms[i].length) {
+        if (lastX <= platforms[i].x + platforms[i].length)
+        {
           lastX = platforms[i].x + platforms[i].length;
         }
       }
@@ -269,18 +290,23 @@ void createPlatform() {
 }
 
 // Move and display platforms
-void updateScene() {
+void updateScene()
+{
   // last X needs to be recalculated after every scene update
   lastX = 0;
 
-  for (int i = 0; i < MAX_PLATFORMS; i++) {
-    if (platforms[i].visible) {
+  for (int i = 0; i < MAX_PLATFORMS; i++)
+  {
+    if (platforms[i].visible)
+    {
       platforms[i].x -= speed;
       // Handle platform disappearing left side
-      if (platforms[i].x + platforms[i].length <= 0) {
+      if (platforms[i].x + platforms[i].length <= 0)
+      {
         platforms[i].visible = false;
       }
-      else if (platforms[i].x + platforms[i].length >= lastX) {
+      else if (platforms[i].x + platforms[i].length >= lastX)
+      {
         lastX = platforms[i].x + platforms[i].length;
       }
 
@@ -290,15 +316,16 @@ void updateScene() {
   }
 }
 
-
-void setup() {
+void setup()
+{
   Wire.begin(SDA_PIN, SCL_PIN); // Emulator
   Serial.begin(9600);
 
   pinMode(pinButtonUp, INPUT_PULLUP);
   pinMode(pinButtonDown, INPUT_PULLUP);
 
-  if (!oled.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+  if (!oled.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
+  {
     Serial.println(F("SSD1306 allocation failed"));
   }
 
@@ -320,10 +347,12 @@ void setup() {
   // oled.fillRect(start.x + 4, start.y + PLATFORM_HEIGHT + 1, CHARACTER_WIDTH, CHARACTER_HEIGHT, WHITE);
 }
 
-void loop() {
+void loop()
+{
 
   // checking the game over condition
-  if (lives <= 0) {
+  if (lives <= 0)
+  {
     gameOver();
     return;
   }
@@ -341,7 +370,7 @@ void loop() {
 
   // Handle character logic
   jump();
-  jumpPhysics(); 
+  jumpPhysics();
   handleFallAndLives();
   drawEnemy();
 
